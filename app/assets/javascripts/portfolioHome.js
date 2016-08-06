1c772d05972ed1e7f47a0b968f1c9711748c75a1
Branislav Bugarcic <bugarcic@gmail.com>
2016-08-07 00:51:13 +0200

    removed marker from map

--- app/assets/javascripts/portfolioHome.js
+++ app/assets/javascripts/portfolioHome.js
@@ -22,22 +22,13 @@
     handler.buildMap({
         internal: {id: 'map'},
         provider: {
-          zoom: 9,
+          zoom: 10,
           center: new google.maps.LatLng(52.5520332, 13.439426),
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           styles: mapStyle
         }
       },
-      function(){
-        marker = handler.addMarker([
-          {
-            "lat": 52.5520332,
-            "lng": 13.439426
-          }
-        ]);
-        handler.bounds.extendWith(marker);
-        handler.fitMapToBounds();
-      }
+      function(){}
     );
   };
 	/*var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];*/
